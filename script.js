b1abfa876f339e5edc885d409916e4e72679bd23
Samir <samir.sattorov01@mail.ru>
2024-07-16 16:51:15 +0300

    finish of homework-16

---
+++ script.js
@@ -0,0 +1,88 @@
+let userName1 = prompt("Как ваше имя ?");
+let userName2 = prompt("Как ваще имя?");
+let userName3 = prompt("Как ваще имя?");
+let userName4 = prompt("Как ваще имя?");
+
+const userNameDoc1 = document.getElementById("userName1");
+userNameDoc1.textContent = userName1;
+
+const userNameDoc2 = document.getElementById("userName2");
+userNameDoc2.textContent = userName2;
+
+const userNameDoc3 = document.getElementById("userName3");
+userNameDoc3.textContent = userName3;
+
+const userNameDoc4 = document.getElementById("userName4");
+userNameDoc4.textContent = userName4;
+
+let finish = alert("Вы успешно зарегистрировались!");
+console.log(finish);
+
+const number = 97;
+const resultBallByNooruz = document.getElementById("nooruz_result");
+resultBallByNooruz.textContent = `  Ваш результат: ${number}/100`;
+
+const number2 = 89;
+const resultBallByZhakshylyk = document.getElementById("zhakshylyk_result");
+resultBallByZhakshylyk.textContent = `  Ваш результат: ${number2}/100`;
+
+const number3 = 75;
+const resultBallByAlisher = document.getElementById("alisher_result");
+resultBallByAlisher.textContent = `  Ваш результат: ${number3}/100`;
+
+const number4 = 90;
+const resultBallByElizar = document.getElementById("elizar_result");
+resultBallByElizar.textContent = `  Ваш результат: ${number4}/100`;
+
+const mnsec = 4;
+const mn1 = 1;
+const mn2 = 2;
+const mn3 = 3;
+const mn4 = 4;
+const mn5 = 5;
+const mn6 = 6;
+const mn7 = 7;
+const mn8 = 8;
+const mn9 = 9;
+const mn10 = 10;
+
+let answer1 = mn1 * mnsec;
+let answer2 = mn2 * mnsec;
+let answer3 = mn3 * mnsec;
+let answer4 = mn4 * mnsec;
+let answer5 = mn5 * mnsec;
+let answer6 = mn6 * mnsec;
+let answer7 = mn7 * mnsec;
+let answer8 = mn8 * mnsec;
+let answer9 = mn9 * mnsec;
+let answer10 = mn10 * mnsec;
+
+let primer1 = "1*4=" + answer1;
+console.log(primer1);
+
+let primer2 = "2*4=" + answer2;
+console.log(primer2);
+
+let primer3 = "3*4=" + answer3;
+console.log(primer3);
+
+let primer4 = "4*4=" + answer4;
+console.log(primer4);
+
+let primer5 = "5*4=" + answer5;
+console.log(primer5);
+
+let primer6 = "6*4=" + answer6;
+console.log(primer6);
+
+let primer7 = "7*4=" + answer7;
+console.log(primer7);
+
+let primer8 = "8*4=" + answer8;
+console.log(primer8);
+
+let primer9 = "9*4=" + answer9;
+console.log(primer9);
+
+let primer10 = "10*4=" + answer10;
+console.log(primer10);
