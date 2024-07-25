e621b3e5927d8e6e7dc8f1d3acd8419c6426408b
Samir <samir.sattorov01@mail.ru>
2024-07-25 17:41:11 +0300

    finish homework-20

--- script.js
+++ script.js
@@ -14,16 +14,75 @@
 // }
 // createStudent(Student1);
 
-const Student2 = {};
-function createStudent2(name, age, university, faculty, gpa) {
-  Student2.name = name;
-  Student2.age = age;
-  Student2.education = {
-    university: university,
-    faculty: faculty,
-    gpa: gpa,
-  };
+//Задача-1
 
-  console.log(Student2);
-}
-createStudent2("Samir", 13, "Osh", "Cs", 87);
+// const Student2 = {};
+// function createStudent2(name, age, university, faculty, gpa) {
+//   Student2.name = name;
+//   Student2.age = age;
+//   Student2.education = {
+//     university: university,
+//     faculty: faculty,
+//     gpa: gpa,
+//   };
+
+//   console.log(Student2);
+// }
+// createStudent2("Samir", 13, "Osh", "Cs", 87);
+
+//Задача-2
+
+// const user = {
+//   email: "samir.sattrov01@mail.ru",
+//   password: "samir-krotoy",
+// };
+
+// function deleteKey(object, key) {
+//   if (key in object) {
+//     delete object[key];
+//     console.log("Key is deleted");
+//   } else {
+//     alert("Ключа нет");
+//   }
+// }
+// deleteKey(user, "password");
+
+//Задание-3
+
+// let proverkaEmail = prompt("Ваш email");
+// let proverkaPassword = prompt("Ваш пароль:");
+// let sum = 5;
+
+// const user1 = {
+//   name: "Adam",
+//   age: 15,
+//   email: "adam01@mail.ru",
+//   password: "123456",
+// };
+
+// if (user1.email === proverkaEmail && user1.password === proverkaPassword) {
+//   let proverkaPrimer = +prompt("2+3=?");
+//   if (proverkaPrimer === sum) {
+//     user1.sum = proverkaPrimer;
+//     alert("Добро пожаловать! Ваша премия " + user1.sum);
+//   } else {
+//     alert("Иди учись!");
+//   }
+// } else if (user1.email !== proverkaEmail) {
+//   alert("Email неверный!");
+// } else if (user1.password !== proverkaPassword) {
+//   alert("Пароль неверный!");
+// }
+
+// const array = [
+//   {
+//     name: "samir",
+//   },
+// ];
+// const newArray = array.map((item) => {
+//   return {
+//     name: item.name,
+//     email: `${item.name}@gmail.com`,
+//   };
+// });
+// console.log(newArray);
